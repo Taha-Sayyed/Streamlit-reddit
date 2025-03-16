109fd62892f827cf52464c12951a7e10e4c3b63b
Taha-Sayyed <codetaha786@gmail.com>
2025-03-16 12:29:28 +0530

    Changes Made

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,19 @@ db = firestore.Client.from_service_account_json("firestore-key.json")
 # Streamlit widgets to let a user create a new post
 title = st.text_input("Post title")
 url = st.text_input("Post url")
+description=st.text_input("Post the description")
 submit = st.button("Submit new post")
 
+
 # Once the user has submitted, upload it to the database
-if title and url and submit:
+if title and url and description and submit:
 	doc_ref = db.collection("posts").document(title)
 	doc_ref.set({
 		"title": title,
-		"url": url
+		"url": url,
+		"description":description
 	})
+
 
 # And then render each post, using some light Markdown
 posts_ref = db.collection("posts")
@@ -22,6 +26,8 @@ for doc in posts_ref.stream():
 	post = doc.to_dict()
 	title = post["title"]
 	url = post["url"]
+	description=post["description"]
 
 	st.subheader(f"Post: {title}")
 	st.write(f":link: [{url}]({url})")
+	st.write(f"Description: {description}")
